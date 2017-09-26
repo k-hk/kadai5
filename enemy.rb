bf5ea8731c61e3e575b225d5235a9445ef27e3d1
k-hk <j1310@PC4-56.cc.matsue-ct.jp>
2017-09-26 17:19:44 +0900

    敵機が自機に向かうようにした

--- enemy.rb
+++ enemy.rb
@@ -1,15 +1,24 @@
 # coding: utf-8
 
 class Enemy < Sprite
+
+  def speed(x,y,v)
+    at = Math.atan2((y - self.y),(x - self.x))
+    @v_x = Math.cos(at) * v
+    @v_y = Math.sin(at) * v
+  end
+
   def update
-    self.y += 1
+    self.y = self.y + @v_y
+    self.x = self.x + @v_x
     if self.y >= Window.height - self.image.height
       self.vanish
     end
   end
 
   # 他のオブジェクトから衝突された際に呼ばれるメソッド
-  def hit(obj)
+=begin  def hit(obj)
     self.vanish
   end
+=end
 end
